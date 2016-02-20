bf821804655958d5b03b7fa2fc40936736cf94ed
Garrett Stot <garrettstott@gmail.com>
2016-02-20 11:44:25 -0700

    "Mostly working. Need to figure out how to add more than one side_dish.cost to total"

--- lunch.rb
+++ lunch.rb
@@ -1,6 +1,7 @@
 # Lunch Lady Land
 require 'pry'
 require 'babbler'
+@babble = Babbler.babble
 
 class Food
   attr_accessor :name, :cost
@@ -10,72 +11,53 @@ class Food
   end
 end
 
-class MainDish < Food
-  def initialize(name, cost)
-    super(name, cost)
-  end
+def total
+  total = @main_dish.cost + @side_dish.cost
 end
 
-class SideDish < Food
-  def initialize(name, cost)
-    super(name, cost)
+def anything_else
+  puts
+  puts "Anything else?"
+  print "> "
+  input_3 = gets.strip
+  if input_3 == 'yes'
+    side_menu
+  elsif input_3 == 'no'
+    puts "Total: $#{total}"
+    exit
   end
 end
-
-
-@babble = Babbler.babble
 
 def main_menu
   puts
   puts '-- Lunch Menu --'
   puts 'Please choose a main dish:'
-  puts '1: Hot Ham Water'
-  puts '2: Cold soup'
-  puts "3: #{@babble}"
+  puts '1: Hot Ham Water ($3)'
+  puts '2: Cold soup ($4)'
+  puts "3: #{@babble} ($5)"
   print "> "
 end
 
 def side_menu
   puts
   puts "Please choose a side:"
-  puts "1: Peas"
-  puts "2: Yellowish stuff"
-  puts "3: Surprise me"
-end
-
-def input_2
-  while(true)
-    side_menu
-    @input_2 = gets.strip
-    if @input_2 == '1'
-      side_dish = SideDish.new('Peas', 1)
-      puts "#{side_dish.name}, you sure? Okay...($#{side_dish.cost})"
-    elsif @input_2 == '2'
-      side_dish = SideDish.new('Yellowish Stuff', 1.5)
-      puts "Great choice. I love the #{side_dish.name} ($#{side_dish.cost})"
-    elsif @input_2 == '3'
-      side_dish = SideDish.new('Surprise me', 2)
-      puts "#{side_dish.name}? Let me check the back ($#{side_dish.cost})"
-    elsif @input_2 == 'exit'
-      exit
-    else
-      puts "Please Select a valid option!"
-      exit
-    end
-  end
+  puts "1: Peas ($1)"
+  puts "2: Yellowish stuff ($1.50)"
+  puts "3: Surprise me ($2)"
+  print "> "
 end
 
 def input
 @input_1 = gets.strip
   if @input_1 == '1'
-    main_dish = MainDish.new('Hot Ham Water', 3)
-    puts "mmm...#{main_dish.name}($#{main_dish.cost})"
+    @main_dish = Food.new('Hot Ham Water', 3)
+    puts "mmm...#{@main_dish.name}($#{@main_dish.cost})"
   elsif @input_1 == '2'
-    main_dish = MainDish.new('cold_soup', 4)
-    puts "Right...#{main_dish.name}($#{main_dish.cost})"
+    @main_dish = Food.new('cold_soup', 4)
+    puts "Right...#{@main_dish.name}($#{@main_dish.cost})"
   elsif @input_1 == '3'
-    main_dish = MainDish.new("#{@babble}", 5)
-    puts "Hmm. #{main_dish.name}, okay! ($#{main_dish.cost})"
+    @main_dish = Food.new("#{@babble}", 5)
+    puts "Hmm. #{@main_dish.name}, okay! ($#{@main_dish.cost})"
   elsif @input_1 == 'exit'
     exit
   else
@@ -85,6 +67,30 @@ def input
   side_menu
   input_2
 end
+
+def input_2
+  while(true)
+    @input_2 = gets.strip
+    if @input_2 == '1'
+      @side_dish = Food.new('Peas', 1)
+      puts "#{@side_dish.name}, you sure? Okay...($#{@side_dish.cost})"
+    elsif @input_2 == '2'
+      @side_dish = Food.new('Yellowish Stuff', 1.5)
+      puts "Great choice. I love the #{@side_dish.name} ($#{@side_dish.cost})"
+    elsif @input_2 == '3'
+      @side_dish = Food.new('Surprise me', 2)
+      puts "#{@side_dish.name}? Let me check the back ($#{@side_dish.cost})"
+    elsif @input_2 == 'exit'
+      exit
+    else
+      puts "Please Select a valid option!"
+      exit
+    end
+    anything_else
+  end
+end
+
+
 
 # total = main_dish.cost + side_dish.cost
 # total = @cost_1 + @cost_2
