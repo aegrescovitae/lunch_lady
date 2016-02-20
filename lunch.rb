3ec8ad2f2a454f08a986ba9e5144554914188a8d
Garrett Stot <garrettstott@gmail.com>
2016-02-20 12:28:02 -0700

    Cleaned up and refactored

--- lunch.rb
+++ lunch.rb
@@ -12,7 +12,7 @@ class Food
 end
 
 def total
-  total = @main_dish.cost + @side_dish.cost
+  total = @main_dish.cost.to_f + @side_dish.cost.to_f
 end
 
 def anything_else
@@ -36,6 +36,7 @@ def main_menu
   puts '2: Cold soup ($4)'
   puts "3: #{@babble} ($5)"
   print "> "
+  input_1
 end
 
 def side_menu
@@ -45,60 +46,47 @@ def side_menu
   puts "2: Yellowish stuff ($1.50)"
   puts "3: Surprise me ($2)"
   print "> "
+  input_2
 end
 
-def input
-@input_1 = gets.strip
-  if @input_1 == '1'
+def input_1
+input_1 = gets.strip
+  if input_1 == '1'
     @main_dish = Food.new('Hot Ham Water', 3)
     puts "mmm...#{@main_dish.name}($#{@main_dish.cost})"
-  elsif @input_1 == '2'
+  elsif input_1 == '2'
     @main_dish = Food.new('cold_soup', 4)
     puts "Right...#{@main_dish.name}($#{@main_dish.cost})"
-  elsif @input_1 == '3'
+  elsif input_1 == '3'
     @main_dish = Food.new("#{@babble}", 5)
     puts "Hmm. #{@main_dish.name}, okay! ($#{@main_dish.cost})"
-  elsif @input_1 == 'exit'
+  elsif input_1 == 'exit'
     exit
   else
     puts "Please Select a valid option!"
     exit
   end
   side_menu
-  input_2
 end
 
 def input_2
-  while(true)
-    @input_2 = gets.strip
-    if @input_2 == '1'
-      @side_dish = Food.new('Peas', 1)
-      puts "#{@side_dish.name}, you sure? Okay...($#{@side_dish.cost})"
-    elsif @input_2 == '2'
-      @side_dish = Food.new('Yellowish Stuff', 1.5)
-      puts "Great choice. I love the #{@side_dish.name} ($#{@side_dish.cost})"
-    elsif @input_2 == '3'
-      @side_dish = Food.new('Surprise me', 2)
-      puts "#{@side_dish.name}? Let me check the back ($#{@side_dish.cost})"
-    elsif @input_2 == 'exit'
-      exit
-    else
-      puts "Please Select a valid option!"
-      exit
-    end
-    anything_else
+  input_2 = gets.strip
+  if input_2 == '1'
+    @side_dish = Food.new('Peas', 1)
+    puts "#{@side_dish.name}, you sure? Okay...($#{@side_dish.cost})"
+  elsif input_2 == '2'
+    @side_dish = Food.new('Yellowish Stuff', 1.5)
+    puts "Great choice. I love the #{@side_dish.name} ($#{@side_dish.cost})"
+  elsif input_2 == '3'
+    @side_dish = Food.new('Surprise me', 2)
+    puts "#{@side_dish.name}? Let me check the back ($#{@side_dish.cost})"
+  elsif input_2 == 'exit'
+    exit
+  else
+    puts "Please Select a valid option!"
+    exit
   end
+  anything_else
 end
 
-
-
-# total = main_dish.cost + side_dish.cost
-# total = @cost_1 + @cost_2
-
-
-
 main_menu
-input
-
-
-#puts total
