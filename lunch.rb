5ee2f42d3a5d05a268cec9620d3edb968b62ffe1
Garrett Stot <garrettstott@gmail.com>
2016-02-20 23:53:42 -0700

    All features are working

--- lunch.rb
+++ lunch.rb
@@ -11,6 +11,18 @@ class Food
   end
 end
 
+class Dish < Food
+  def initialize(name, cost)
+    super(name, cost)
+  end
+
+  def calories
+    calories = {hot_ham_water: 40, cold_soup: 50, surprise_me: 400, peas: 20, yellowish_stuff: 650, babble: 2}
+  end
+end
+
+
+
 def main_menu
   puts
   puts ' -- WELCOME --'
@@ -38,14 +50,14 @@ end
 def input_1
 input_1 = gets.strip
   if input_1 == '1'
-    @dish = Food.new('Hot Ham Water', 3.45)
+    @dish = Dish.new('hot_ham_water', 3.45)
     puts "mmm...#{@dish.name}($#{@dish.cost})"
   elsif input_1 == '2'
-    @dish = Food.new('cold_soup', 4.32)
+    @dish = Dish.new('cold_soup', 4.32)
     puts "Right...#{@dish.name}($#{@dish.cost})"
   elsif input_1 == '3'
-    @dish = Food.new("#{@babble}", 5.87)
-    puts "Hmm. #{@dish.name}, okay! ($#{@dish.cost})"
+    @dish = Dish.new('babble', 5.87)
+    puts "Hmm. #{@babble}, okay! ($#{@dish.cost})"
   elsif input_1 == 'clear'
     clear
   elsif input_1 == 'exit'
@@ -61,13 +73,13 @@ end
 def input_2
   input_2 = gets.strip
   if input_2 == '1'
-    @dish = Food.new('Peas', 0.99)
+    @dish = Dish.new('Peas', 0.99)
     puts "#{@dish.name}, you sure? Okay...($#{@dish.cost})"
   elsif input_2 == '2'
-    @dish = Food.new('Yellowish Stuff', 1.5)
+    @dish = Dish.new('yellowish_stuff', 1.5)
     puts "Great choice. I love the #{@dish.name} ($#{@dish.cost})"
   elsif input_2 == '3'
-    @dish = Food.new('Surprise me', 2.12)
+    @dish = Dish.new('surprise_me', 2.12)
     puts "#{@dish.name}? Let me check the back ($#{@dish.cost})"
   elsif input_2 == 'clear'
     clear
@@ -95,7 +107,8 @@ def anything_else
       side_menu
     end
   elsif ['n', 'no'].include?(input_3)
-    puts "Total: $#{check_total}, Change: $#{change}"
+    puts_total
+    check_wallet
     exit
   elsif input_3 == 'clear'
     clear
@@ -107,7 +120,7 @@ def remove_item
   input = gets.strip
   if ['y', 'yes'].include?(input)
     @total_arr.pop
-    puts "Total: $#{check_total}, Change: $#{change}"
+    puts_total
   elsif ['no', 'n'].include?(input)
     puts "Go get more money."
     clear
@@ -125,8 +138,12 @@ def check_total
   @check_total = @total_arr.inject(0){|sum,x| sum + x }
 end
 
+def puts_total
+  @puts_total = puts "Total: $#{check_total}, Change: $#{change}"
+end
+
 def change
-  @change = @wallet - @check_total
+  @change = wallet - check_total
 end
 
 @total_arr = []
@@ -138,5 +155,13 @@ end
 def clear
   system("ruby ~/Documents/DPL/week01/day5/lunch_lady/lunch.rb")
 end
+
+def check_wallet
+  if check_total > wallet
+    puts "You don't have enough money."
+    remove_item
+  end
+end
+
 
 main_menu
